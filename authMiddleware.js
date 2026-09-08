d5e68c7e755d0c8584f246f9985a1d7042aa7064
Hariom Patidar <hp3962261@gmail.com>
2026-09-08 10:12:54 +0530

    Fix Vercel middleware conflict

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,5 +1,5 @@
-const Listing = require("./models/listing");
-const Review = require("./models/review");
+const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 
